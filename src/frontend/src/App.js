7477cbcda53aced5d3eefcb200dc5b37a9fd4996
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-07-06 19:13:01 +0530

    Navbar bit better

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import {Navbar} from "./components/Navbar/Navbar";
 import {Route, Routes} from "react-router";
 import {Home} from "./pages/Home";
+import {NotFound} from "./pages/NotFound";
 
 function App() {
     return (
         <div className="App">
             <Navbar/>
             <Routes>
-                <Route exact path="/" element={<Home />} />
+                <Route index element={<Home />} />
+                <Route exact path="/home" element={<Home />} />
+                <Route exact path="*" element={<NotFound />} />
             </Routes>
         </div>
     );
---
+++ src/frontend/src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NotFound = ()=>{
+    return (
+        <div>
+            404 page not found
+        </div>
+    )
+}
