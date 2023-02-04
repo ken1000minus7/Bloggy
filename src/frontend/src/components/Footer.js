a32f2ea018e60be1f07c87f78fe46983bbd9c615
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-02-05 03:26:08 +0530

    fixed footer at bottom of the page

--- src/frontend/src/components/Footer.js
+++ src/frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <>
-      <footer className="bg-gray-800 text-white py-4">
+      <footer className="bg-gray-800 text-white py-4 absolute bottom-0 w-[-webkit-fill-available]">
         <div className="container mx-auto flex items-center justify-between px-4">
           <p className="text-sm">Copyright © 2022 Bloggy</p>
           <div className="flex">
