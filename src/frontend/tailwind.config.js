543f28ac1339b3dba5d880b817faa728f4f31943
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-02-08 00:22:55 +0530

    Corrected indentation 2

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.js"],
-    theme: {
-        extend: {
-            fontFamily: {
-                itim: ["Itim", "cursive"],
-                SourceSansPro: ["Source Sans Pro", "cursive"],
-            },
-        },
-        screens: {
-            "2xl": { max: "1535px" },
-
-            xl: { max: "1279px" },
-
-            lg: { max: "1023px" },
-
-            md: { max: "767px" },
-
-            sm: { max: "639px" },
-        },
+  content: ["./src/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        itim: ["Itim", "cursive"],
+        SourceSansPro: ["Source Sans Pro", "cursive"],
+      },
     },
-    plugins: [],
+    screens: {
+      "2xl": { max: "1535px" },
+
+      xl: { max: "1279px" },
+
+      lg: { max: "1023px" },
+
+      md: { max: "767px" },
+
+      sm: { max: "639px" },
+    },
+  },
+  plugins: [],
 };
