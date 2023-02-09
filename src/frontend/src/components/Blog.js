3e98a44f8b17794ce385b8a7ddb0d2e115377917
ugpec79 <ujjawalguptapec@gmail.com>
2023-02-09 23:22:15 +0530

    Update the Blog

--- src/frontend/src/components/Blog.js
+++ src/frontend/src/components/Blog.js
@@ -76,7 +76,9 @@ export const Blog = ({ blog }) => {
                                 else{
                                     navigate('/login')
                                 }
-                }}>Update The Blog</button>
+                }} class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                Update The Blog
+                </button>
             </div> ) : ( <></> )
       }
     </Fade>
