38a8ab42adb0edce3cd1989b76d1f55b80539d56
ugpec79 <113056503+ugpec79@users.noreply.github.com>
2023-02-09 23:42:38 +0530

    Updated 1

--- src/frontend/src/pages/UpdatePage.js
+++ src/frontend/src/pages/UpdatePage.js
@@ -6,6 +6,7 @@ import {LoadingDialog} from "../components/LoadingDialog";
 
 export const BlogPage = ()=>{
 
+    const {blogid} = useParams()
     const {id} = useParams()
     const [blog,setBlog] = useState(null)
     const [loading,setLoading] = useState(false)
@@ -55,7 +56,7 @@ export const BlogPage = ()=>{
             return
         }
         axios({
-            url : `${process.env.REACT_APP_API_BASE_URL}/blog/${id}`,
+            url : `${process.env.REACT_APP_API_BASE_URL}/blog/${blogid}`,
             method : "PUT",
             headers : {
                 "Content-Type" : "application/json",
