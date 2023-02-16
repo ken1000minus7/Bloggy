81bd58b3e8acd2d507c15145f4f8d94bb42287db
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-02-17 01:09:47 +0530

    Changed initial values of useState  windowSize

--- src/frontend/src/hooks/useWindowSize.js
+++ src/frontend/src/hooks/useWindowSize.js
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 function useWindowSize() {
 
     const [windowSize, setWindowSize] = useState({
-      width: undefined,
-      height: undefined,
+      width: window.innerWidth,
+      height: window.innerHeight,
     });
     useEffect(() => {
       
