51c7a1d94517b4c6a48904ac9ce56d210dc1a82c
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-02-20 22:36:14 +0530

    Added upvote downvote component

--- src/frontend/src/components/Comment.js
+++ src/frontend/src/components/Comment.js
@@ -18,6 +18,20 @@ const Comment = ({comment}) => {
           </div>
         </div>
       </div>
+
+      {/* Upvote Downvote component */}
+      <button type="button" class="-rotate-90 border border-green-700 hover:bg-green-500 rounded-md p-2.5    inline-flex items-center  hover:text-white mr-4 hover:scale-[1.05] hover:duration-200">
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+        </svg>
+      </button>
+      <button type="button" class="rotate-90 border border-red-700 hover:bg-red-500 rounded-md p-2.5  inline-flex items-center hover:text-white hover:scale-[1.05] hover:duration-200">
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"clip-rule="evenodd"></path>
+        </svg>
+      </button>
+
     </div>
   </div>
 
