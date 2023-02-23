4afcaddd063032c0f00261db1e6692b281531bec
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-02-24 00:04:37 +0530

    Added upvote downvote in comments

--- src/frontend/src/components/Comment.js
+++ src/frontend/src/components/Comment.js
@@ -6,11 +6,11 @@ const Comment = ({comment}) => {
         <>
 
   <div className="px-5">
-    <div className="bg-[#ede7f6] max-w-xl rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
+    <div className="bg-[#ede7f6] max-w-xl rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500 blogCardAuthor">
       <div className="mt-4">
         <p className="mt-4 text-md text-gray-600 font-normal">{content}</p>
         <div className="flex justify-between items-center">
-          <div className="mt-4 flex items-center space-x-4 py-6">
+          <div className="mt-4 flex items-center space-x-4 pt-6">
             <div class="">
               <img class="w-12 h-12 rounded-full" src={fileImagePath} alt={authorUsername} />
             </div>
@@ -20,17 +20,19 @@ const Comment = ({comment}) => {
       </div>
 
       {/* Upvote Downvote component */}
-      <button type="button" class="-rotate-90 border border-green-700 hover:bg-green-500 rounded-md p-2.5    inline-flex items-center  hover:text-white mr-4 hover:scale-[1.05] hover:duration-200">
-        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-        </svg>
-      </button>
-      <button type="button" class="rotate-90 border border-red-700 hover:bg-red-500 rounded-md p-2.5  inline-flex items-center hover:text-white hover:scale-[1.05] hover:duration-200">
-        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+      <div className="buttonContainer">
+        <button type="button" class="h-[21px] w-[22px] -rotate-90 border-2 border-green-700 hover:bg-green-600 rounded-md p-[0.25rem]  inline-flex items-center  hover:text-white  hover:scale-[1.05] hover:duration-200">
+            <svg aria-hidden="true" class="w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+            </svg>
+          </button>
+          <button type="button" class="h-[21px] w-[22px] rotate-90 border-2 border-red-700 hover:bg-red-600 rounded-md p-[0.25rem]  inline-flex items-center hover:text-white hover:scale-[1.05] hover:duration-200">
+            <svg aria-hidden="true" class="w-5" fill="currentColor" viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"clip-rule="evenodd"></path>
-        </svg>
-      </button>
+              <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"clip-rule="evenodd"></path>
+            </svg>
+          </button>
+      </div>
 
     </div>
   </div>
